7bf6b571f4563b9de07865c9923a60aca204233e
aleksandarkraljevic <a.kraljevic.2000@hotmail.com>
2024-01-30 17:19:46 +0100

    Work work
    Working on making the whole code structured and experiment ready for cartpole.

---
+++ helper.py
@@ -0,0 +1,251 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from collections import deque
+from scipy.signal import savgol_filter
+from pqc import *
+
+def exponential_anneal(t, start, final, decay_constant):
+    '''
+    Exponential annealing scheduler for epsilon-greedy policy.
+
+    Parameters
+    ----------
+    t (int):
+        Current timestep.
+    start (float):
+        Initial epsilon value.
+    final (float):
+        Lowest possible value that epsilon can get to.
+    decay_constant (float):
+        The speed at which epsilon decays.
+    '''
+    return final + (start - final) * np.exp(-decay_constant*t)
+
+
+def boltzmann_exploration(actions, temperature):
+    '''
+    Boltzmann exploration policy.
+
+    Parameters
+    ----------
+    actions (list):
+        Vector with possible actions.
+    temperature (float):
+        Strength of the exploration.
+    '''
+    actions = actions[0] - np.max(actions[0])
+    a = actions / temperature  # scale by temperature
+    return np.exp(a)/np.sum(np.exp(a))
+
+def plot(data_name, show, savename, smooth):
+    '''
+    Plots model training data.
+
+    Parameters
+    ----------
+    data_name (str):
+        The name of the data file, excluding the file extension.
+    show (boolean):
+        Whether the plot will be shown to the user.
+    savename (str):
+        What name the plot will be saved as. If False, then the plot is not saved.
+    smooth (boolean):
+        Whether savgol smoothing will be applied or not.
+    '''
+    data = np.load('data/'+data_name+'.npy', allow_pickle=True)
+    rewards = data.item().get('rewards')
+    n_holes = data.item().get('n_holes')
+    memory_size = 2 * (n_holes - 2)
+    if smooth==True:
+        rewards = savgol_filter(rewards, 71, 1)
+    episodes = np.arange(1, len(rewards) + 1)
+    dataframe = np.vstack((rewards, episodes)).transpose()
+    dataframe = pd.DataFrame(data=dataframe, columns=['Reward', 'Episode'])
+    plt.figure()
+    sns.set_theme()
+    sns.lineplot(data=dataframe, x='Episode', y='Reward')
+    plt.ylim(-1 * memory_size, 0)
+    plt.title('Reward per episode')
+    if savename != False:
+        plt.savefig('plots/'+savename+'.png')
+    if show:
+        plt.show()
+
+def plot_averaged(data_names, show, savename, smooth):
+    '''
+    Plots an experiment's training average over all of its repetitions, including its standard errors.
+
+    Parameters
+    ----------
+    data_names (list):
+        A list of the data file names, excluding the file extensions.
+    show (boolean):
+        Whether the plot will be shown to the user.
+    savename (str):
+        What name the plot will be saved as. If False, then the plot is not saved.
+    smooth (boolean):
+        Whether savgol smoothing will be applied or not.
+    '''
+    n_names = len(data_names)
+    data = np.load('data/'+data_names[0]+'.npy', allow_pickle=True)
+    n_holes = data.item().get('n_holes')
+    memory_size = 2 * (n_holes - 2)
+    rewards = data.item().get('rewards')
+    episodes = np.arange(1, len(rewards) + 1)
+    for i in range(n_names-1):
+        data =  np.load('data/'+data_names[i+1]+'.npy', allow_pickle=True)
+        new_rewards = data.item().get('rewards')
+        rewards = np.vstack((rewards, new_rewards))
+    mean_rewards = np.mean(rewards, axis=0)
+    se_rewards = np.std(rewards, axis=0) / np.sqrt(n_names) # standard error
+    lower_bound = np.clip(mean_rewards-se_rewards, None , 0)
+    upper_bound = np.clip(mean_rewards+se_rewards, None, 0)
+    if smooth == True:
+        mean_rewards = savgol_filter(mean_rewards, 71, 1)
+    dataframe = np.vstack((mean_rewards, episodes)).transpose()
+    dataframe = pd.DataFrame(data=dataframe, columns=['Reward', 'Episode'])
+
+    plt.figure()
+    sns.set_theme()
+    sns.lineplot(data=dataframe, x='Episode', y='Reward')
+    plt.fill_between(episodes, lower_bound, upper_bound, color='b', alpha=0.2)
+    plt.ylim(-1*memory_size,0)
+    plt.title('Mean reward per episode')
+    if savename != False:
+        plt.savefig('plots/'+savename+'.png')
+    if show:
+        plt.show()
+
+def compare_models(parameter_names, repetitions, show, savename, label_names, smooth):
+    '''
+    Plots multiple experiments' averaged training with their standard errors.
+
+    Parameters
+    ----------
+    parameter_names (list):
+        The list of the various experiments' parameter names, excluding "-repetition_" onwards.
+    repetitions (int):
+        The number of repetitions that each experiment contains.
+    show (boolean):
+        Whether the plot will be shown to the user.
+    savename (str):
+        What name the plot will be saved as. If False, then the plot is not saved.
+    label_names (list):
+        A list of strings representing the label name of each experiment in the plot's legend.
+    smooth (boolean):
+        Whether savgol smoothing will be applied or not.
+    '''
+    # this function requires the user to put all the experiment data in the data folder
+    colors_list = ['blue', 'orange', 'green', 'red', 'purple', 'brown']
+    plt.figure()
+    sns.set_theme()
+
+    for experiment in range(len(parameter_names)):
+        data = np.load('data/'+parameter_names[experiment]+'-repetition_1.npy', allow_pickle=True)
+        n_holes = data.item().get('n_holes')
+        memory_size = 2*(n_holes-2)
+        rewards = data.item().get('rewards')
+        episodes = np.arange(1, len(rewards) + 1)
+        for i in range(repetitions-1):
+            data = np.load('data/'+parameter_names[experiment]+'-repetition_'+str(i+2)+'.npy', allow_pickle=True)
+            new_rewards = data.item().get('rewards')
+            rewards = np.vstack((rewards, new_rewards))
+        mean_rewards = np.mean(rewards, axis=0)
+        se_rewards = np.std(rewards, axis=0) / np.sqrt(repetitions)  # standard error
+        lower_bound = np.clip(mean_rewards - se_rewards, None, 0)
+        upper_bound = np.clip(mean_rewards + se_rewards, None, 0)
+        if smooth == True:
+            mean_rewards = savgol_filter(mean_rewards, 71, 1)
+        dataframe = np.vstack((mean_rewards, episodes)).transpose()
+        dataframe = pd.DataFrame(data=dataframe, columns=['Reward', 'Episode'])
+
+        sns.lineplot(data=dataframe, x='Episode', y='Reward', label=label_names[experiment])
+        plt.fill_between(episodes, lower_bound, upper_bound, color=colors_list[experiment], alpha=0.1)
+        plt.ylim(-1 * memory_size, 0)
+
+    plt.title('Mean reward per episode')
+    if savename != False:
+        plt.savefig('plots/' + savename + '.png')
+    if show:
+        plt.show()
+
+def evaluate(model_name, n_samples, print_strategy, print_evaluation, plot_distribution, save):
+    '''
+    Evaluates a single model by looking at its best-found strategy, and averaging its performance. It also plots the distribution of all the numbers of needed guesses after averaging.
+
+    Parameters
+    ----------
+    model_name (str):
+        The name of the model file, excluding the file extension.
+    n_samples (int):
+        The amount of samples that the model will be averaged over.
+    print_strategy (boolean):
+        Whether the strategy of the model will be printed.
+    print_evaluation (boolean):
+        Whether the performance of the model will be printed.
+    plot_distribution (boolean):
+        Whether the distribution of the numbers of needed guesses of all samples will be plotted.
+    save (boolean):
+        Whether the plots will be saved using the same name as parameter_name.
+    '''
+    data = np.load('data/'+model_name+'.npy', allow_pickle=True)
+    n_holes = data.item().get('n_holes')
+    n_layers = data.item().get('n_layers')
+    memory_size = 2 * (n_holes - 2)
+    qubits = cirq.GridQubit.rect(1, memory_size)
+    ops = [cirq.Z(q) for q in qubits]
+    observables = []
+    for i in range(n_holes):
+        observables.append(ops[i])
+    quantum_model = QuantumModel(qubits, n_layers, observables)
+    model = quantum_model.generate_model_Qlearning(False)
+    model.load_weights('models/' + model_name)
+    env = FoxInAHole(n_holes)
+    episode_lengths = []
+    episode_rewards = []
+    if print_strategy:
+        done = env.reset()
+        state = deque([0]*memory_size, maxlen=memory_size)
+        for step in range(memory_size):
+            state_tensor = tf.convert_to_tensor([np.array(state)])
+            q_vals = model([state_tensor])
+            action = np.argmax(q_vals) + 1
+            state.append(action)
+        print("The strategy for the first 2(n-2) guesses =", list(state))
+    for sample in range(n_samples):
+        current_episode_length = 0
+        episode_reward = 0
+        done = env.reset()
+        state = deque([0]*memory_size, maxlen=memory_size)
+        while not done:
+            current_episode_length += 1
+            state_tensor = tf.convert_to_tensor([np.array(state)])
+            q_vals = model([state_tensor])
+            action = np.argmax(q_vals)
+            reward, done = env.guess(action)
+            episode_reward += reward
+            new_observation = state.copy()
+            new_observation.append(action)
+            state = new_observation
+            env.step()
+        episode_lengths.append(current_episode_length)
+        episode_rewards.append(episode_reward)
+
+    if print_evaluation:
+        print('The average amount of guesses needed to finish the game is:', round(np.mean(episode_lengths), 2), '+-',
+              round(np.std(episode_lengths) / np.sqrt(n_samples), 2))
+        print('The average reward per game is:', round(np.mean(episode_rewards), 2), '+-',
+              round(np.std(episode_rewards) / np.sqrt(n_samples), 2))
+
+    if plot_distribution:
+        episode_rewards = [x * (-1) + 1 for x in episode_rewards]
+        episode_rewards = pd.DataFrame(episode_rewards, columns=["# guesses"])
+        plt.figure()
+        sns.histplot(episode_rewards, x="# guesses")
+        plt.title("Distribution of the guess count")
+        if save:
+            plt.savefig('plots/' + model_name + '-distribution.png')
+        plt.show()
+
+    return np.mean(episode_lengths)
---
+++ quantum_model.py
@@ -0,0 +1,316 @@
+import importlib, pkg_resources
+importlib.reload(pkg_resources)
+
+from helper import *
+
+import cirq
+import time
+import numpy as np
+from collections import deque
+tf.get_logger().setLevel('ERROR')
+
+class QDQN():
+    def __init__(self, savename, model, model_target, n_layers, n_holes, qubits, memory_size, learning_rates, gamma, n_episodes, steps_per_train, soft_weight_update, steps_per_target_update, tau, epsilon_start, epsilon_min, decay_epsilon, temperature, batch_size, min_size_buffer, max_size_buffer, exploration_strategy):
+        '''
+        Initializes the QDQN parameters.
+
+        Parameters
+        ----------
+        savename (str):
+            The name with which the file model and data files will be saved.
+        model (tensorflow keras model):
+            The base network.
+        model_target (tensorflow keras model):
+            The target network.
+        n_layers (int):
+            The number of layers that the PQC will contain.
+        n_holes (int):
+            Number of outputs of the DNN. / Number of holes in the environment.
+        qubits (int):
+            The number of qubits that the PQC will use.
+        memory_size (int):
+            The amount of guesses that the agent is allowed to look back. / The state-space size.
+        learning_rates (list):
+            A list of three learning rates that the optimizers within the PQC use in order to update the encoding, variational, and rescaling weights.
+        gamma (float):
+            The discount factor that is used in the Q-learning algorithm.
+        n_episodes (int):
+            The amount of total episodes that the model will train for.
+        steps_per_train (int):
+            The amount of time steps that pass in between each training step.
+        soft_weight_update (boolean):
+            Whether the target network will be updated via soft-updating. If False, then it will be hard-updating.
+        steps_per_target_update (int):
+            Per how many training steps the target network will update, if it is hard-updating.
+        tau (float):
+            The fraction with which the base network copies over to the target network after each training step.
+        epsilon_start (float):
+            The starting value of epsilon in the case that annealing epsilon-greedy is used.
+        epsilon_min (float):
+            The lowest value of epsilon in the case that annealing epsilon-greedy is used.
+        decay_epsilon (float):
+            How fast epsilon decays in the case that annealing epsilon-greedy is used.
+        temperature (float):
+            The strength with which exploration finds place in the case that the Boltzmann policy is used.
+        batch_size (int):
+            The amount of samples that each training batch consists of.
+        min_size_buffer (int):
+            The minimum size that the experience replay buffer needs to be before training may start.
+        max_size_buffer (int):
+            The maximum size that the experience replay buffer is allowed to be. If this limit is reached then the oldest samples start being replaced with the newest samples.
+        exploration_strategy (str):
+            What exploration strategy should be followed during the training of the model. Either "egreedy" or "boltzmann".
+        '''
+        self.savename = savename
+        self.model = model
+        self.model_target = model_target
+        self.n_layers = n_layers
+        self.n_holes = n_holes
+        self.qubits = qubits
+        self.memory_size = memory_size
+        self.gamma = gamma
+        self.n_episodes = n_episodes
+        self.steps_per_train = steps_per_train
+        self.soft_weight_update = soft_weight_update
+        self.steps_per_target_update = steps_per_target_update
+        self.tau = tau
+        self.epsilon_start = epsilon_start
+        self.epsilon_min = epsilon_min
+        self.decay_epsilon = decay_epsilon
+        self.temperature = temperature
+        self.batch_size = batch_size
+        self.min_size_buffer = min_size_buffer
+        self.max_size_buffer = max_size_buffer
+        self.exploration_strategy = exploration_strategy
+        self.optimizer_in = tf.keras.optimizers.Adam(learning_rate=learning_rates[0], amsgrad=True)
+        self.optimizer_var = tf.keras.optimizers.Adam(learning_rate=learning_rates[1], amsgrad=True)
+        self.optimizer_out = tf.keras.optimizers.Adam(learning_rate=learning_rates[2], amsgrad=True)
+        # Indexes of the weights for each of the parts of the circuit
+        self.w_in, self.w_var, self.w_out = 1, 0, 2
+
+    def main(self):
+        '''
+        Handles the main bulk of the QDQN, making use of all the other functions in this class.
+        '''
+        env = FoxInAHole(self.n_holes)
+        replay_memory = deque(maxlen=self.max_size_buffer)
+
+        episode_reward_history = []
+        episode_length_history = []
+        step_count = 0
+
+        for episode in range(self.n_episodes):
+            episode_reward = 0
+            state = deque([0] * self.memory_size, maxlen=self.memory_size)
+            done = env.reset()
+            episode_length = 0
+
+            # annealing, done before the while loop because the first episode equals 0 so it returns the original epsilon back
+            if self.exploration_strategy == 'egreedy':
+                epsilon = exponential_anneal(episode, self.epsilon_start, self.epsilon_min, self.decay_epsilon)
+
+            while True:
+                episode_length += 1
+
+                state_tensor = tf.convert_to_tensor([np.array(state)])
+                q_vals = self.model([state_tensor])
+                possible_actions = np.arange(self.n_holes)
+
+                # Sample action
+                if self.exploration_strategy == 'egreedy':
+                    coin = np.random.random()
+                    if coin > epsilon:
+                        action = int(tf.argmax(q_vals[0]).numpy())
+                    else:
+                        action = np.random.randint(self.n_holes)
+                elif self.exploration_strategy == 'boltzmann':
+                    probabilities = boltzmann_exploration(np.array(q_vals), self.temperature)
+                    action = np.random.choice(possible_actions, p=probabilities)
+
+                # Interact with env
+                interaction = self.interact_env(state, action, env)
+
+                # Store interaction in the replay memory
+                replay_memory.append(interaction)
+
+                state = interaction['next_state']
+                episode_reward += interaction['reward']
+                step_count += 1
+
+                # Update model
+                if step_count % self.steps_per_train == 0 and len(replay_memory) >= self.min_size_buffer:
+                    # Sample a batch of interactions and update Q_function
+                    training_batch = np.random.choice(replay_memory, size=self.batch_size)
+                    self.Q_learning_update(np.asarray([x['state'] for x in training_batch]),
+                                      np.asarray([x['action'] for x in training_batch]),
+                                      np.asarray([x['reward'] for x in training_batch], dtype=np.float32),
+                                      np.asarray([x['next_state'] for x in training_batch]),
+                                      np.asarray([x['done'] for x in training_batch], dtype=np.float32))
+
+                # Update target model
+                if self.soft_weight_update:
+                    self.update_model()
+                elif step_count % self.steps_per_target_update == 0:
+                    self.update_model()
+
+                # Check if the episode is finished
+                if interaction['done']:
+                    break
+
+                env.step()
+
+            episode_length_history.append(episode_length)
+            episode_reward_history.append(episode_reward)
+
+            if episode % 100 == 0:
+                print('Training progress: ' + str(episode) + '/' + str(self.n_episodes))
+
+        if self.savename != False:
+            self.save_data(episode_reward_history, episode_length_history)
+
+    def update_model(self):
+        '''
+        Copies weights from the base network to the network via a hard-update or soft-update rule.
+        '''
+        if self.soft_weight_update:
+            new_weights = []
+            for TN_layer, BM_layer in zip(self.model_target.get_weights(), self.model.get_weights()):
+                new_weights.append((1-self.tau) * TN_layer + self.tau * BM_layer)
+            self.model_target.set_weights(new_weights)
+        else:
+            self.model_target.set_weights(self.model.get_weights())
+
+    def save_data(self, rewards, episode_lengths):
+        '''
+        Saves the model after its training, as well as important results and properties.
+
+        Parameters
+        ----------
+        rewards (list):
+            A list of all the rewards that were obtained at the end of each episode.
+        episode_lengths (list):
+            A list of the length of each episode.
+        '''
+        data = {'n_holes': self.n_holes, 'rewards': rewards, 'episode_lengths': episode_lengths, 'n_layers': self.n_layers}
+        np.save('data/' + self.savename + '.npy', data)
+        self.model_target.save_weights('models/' + self.savename)
+
+    def interact_env(self, state, action, env):
+        '''
+        Apply sampled action in the environment, receives reward and next state.
+
+        Parameters
+        ----------
+        state (tensor):
+            The state that the agent is in.
+        action (int):
+            The action that the agent decides to take.
+        env (class):
+            The environment that the agent performs an action in.
+        '''
+        reward, done = env.guess(action)
+        next_state = state.copy()
+        next_state.append(action)
+
+        interaction = {'state': state, 'action': action, 'next_state': next_state,
+                       'reward': reward, 'done':np.float32(done)}
+
+        return interaction
+
+    @tf.function
+    def Q_learning_update(self, states, actions, rewards, next_states, done):
+        '''
+        Trains the QDQN model.
+
+        Parameters
+        ----------
+        states (array):
+            An array of all the states of the samples that will be used for training.
+        actions (array):
+            An array of all the actions of the samples that will be used for training.
+        rewards (array):
+            An array of all the rewards of the samples that will be used for training.
+        next_states (array):
+            An array of all the states of the samples after the corresponding action has been taken.
+        done (array):
+            An array of whether each sample's next_state was the final state before the game ended.
+        '''
+        states = tf.convert_to_tensor(states)
+        actions = tf.convert_to_tensor(actions)
+        rewards = tf.convert_to_tensor(rewards)
+        next_states = tf.convert_to_tensor(next_states)
+        done = tf.convert_to_tensor(done)
+
+        # Compute their target q_values and the masks on sampled actions
+        future_rewards = self.model_target([next_states])
+        target_q_values = rewards + (self.gamma * tf.reduce_max(future_rewards, axis=1)
+                                                       * (1.0 - done))
+        masks = tf.one_hot(actions, self.n_holes)
+
+        # Train the model on the states and target Q-values
+        with tf.GradientTape() as tape:
+            tape.watch(self.model.trainable_variables)
+            q_values = self.model([states])
+            q_values_masked = tf.reduce_sum(tf.multiply(q_values, masks), axis=1)
+            loss = tf.keras.losses.MeanSquaredError()(target_q_values, q_values_masked)
+
+        # Backpropagation
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        for optimizer, w in zip([self.optimizer_in, self.optimizer_var, self.optimizer_out], [self.w_in, self.w_var, self.w_out]):
+            optimizer.apply_gradients([(grads[w], self.model.trainable_variables[w])])
+
+def main():
+    '''
+    Initializes all the hyperparameters, creates the base and target network by calling upon dnn.py, and trains and saves the model by calling upon the DQN() class.
+    '''
+    n_holes = 5
+    memory_size = 2 * (n_holes - 2)
+    n_qubits = memory_size
+    n_layers = 7  # Number of layers in the PQC
+
+    qubits = cirq.GridQubit.rect(1, n_qubits)
+    ops = [cirq.Z(q) for q in qubits]
+    observables = []
+    for i in range(n_holes):
+        observables.append(ops[i])
+
+    n_episodes = 5000
+    learning_rates = [0.0001, 0.01, 0.1]
+    gamma = 1
+
+    # Define replay memory
+    max_size_buffer = 10000 # Maximum replay length
+    min_size_buffer = 1000
+
+    epsilon_start = 1.0  # Epsilon greedy parameter
+    epsilon_min = 0.01  # Minimum epsilon greedy parameter
+    decay_epsilon = 0.01 # Decay rate of epsilon greedy parameter
+    temperature = 0.01 # Temperature parameter for the Boltzmann exploration
+    batch_size = 64
+    steps_per_train = 10 # Train the model every x steps
+    steps_per_target_update = 10 # parameter for hard updating target network weights
+    tau = 0.05 # parameter for soft updating target network weights
+    soft_weight_update = True # boolean that decides whether to soft update or hard update the target network weights
+    exploration_strategy = 'egreedy' # 'egreedy' or 'boltzmann'
+
+    savename = 'test'
+
+    start = time.time()
+
+    quantum_model = QuantumModel(qubits, n_layers, observables)
+
+    model = quantum_model.generate_model_Qlearning(False)
+    model_target = quantum_model.generate_model_Qlearning(True)
+
+    model_target.set_weights(model.get_weights())
+
+    qdqn = QDQN(savename, model, model_target, n_layers, n_holes, qubits, memory_size, learning_rates, gamma, n_episodes, steps_per_train, soft_weight_update, steps_per_target_update, tau, epsilon_start, epsilon_min, decay_epsilon, temperature, batch_size, min_size_buffer, max_size_buffer, exploration_strategy)
+
+    qdqn.main()
+
+    end = time.time()
+
+    print('Total time: {} seconds (number of episodes: {})'.format(round(end - start, 1), n_episodes))
+
+if __name__ == '__main__':
+    main()
